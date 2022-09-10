7610dc26c7127ea2b9ab4074944349c8256474e0
Prashant <imprashantmodi21@gmail.com>
2022-09-10 10:50:27 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+burger = document.querySelector('#hamburger')
+navbar = document.querySelector('.navbar')
+
+
+ 
+
+
+burger.addEventListener('click', ()=>{
+    
+    navbar.classList.toggle('visibilty');
+    
+    
+})
